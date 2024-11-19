eecdd49eac91a5da53d1a3e5c788fffe57948433
Albert Ibragimov <albert.ibragimov@gmail.com>
2024-11-19 19:37:36 +0500

    Изменили строку вывода

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ print('Hello world!')
 def foo(x: int) -> Tuple[int, float]:
     return x, math.pi
 
-pprint.pprint('Hello!!!')
+
+pprint.pprint('Курс Разработка телеграм-бота на библиотеке aiogram')
 
 print(foo(4))
